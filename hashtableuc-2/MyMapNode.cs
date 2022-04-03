511a446c457f4fa785c8acaf5fdd46310ba96034
srikala <vlslaksh1899@gmail.com>
2022-04-03 23:07:48 +0530

    added

---
+++ hashtableuc-2/MyMapNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HashTable
+{
+    internal class MyMapNode<T1, T2>
+    {
+        private int v;
+
+        public MyMapNode(int v)
+        {
+            this.v = v;
+        }
+
+        internal void Add(string v1, string v2) => throw new NotImplementedException();
+    }
+}
---
+++ hashtableuc-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table Program. \n");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(18);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "ara");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "delibertely");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+
+
+        }
+    }
+}
